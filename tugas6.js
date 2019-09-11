6f3c3ae09fdbc357cf860b2011ca357fa4a8285c
STR <saturasstotenkopf@gmail.com>
2019-09-11 23:57:29 +0700

    Tugas 6 Javascript
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas6.js
@@ -0,0 +1,11 @@
+function perulangan() {
+  for(var i = 0; i<=20; i++){
+    if (i%2 === 0) {
+      console.log("Bilangan Genap");
+    }
+    else {
+      console.log(i);
+    }
+  }
+}
+perulangan()
